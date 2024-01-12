b987d1e9ecd90d08b36a2079c05779c0a08af42f
Hsu Myat Phoo <sumyatphoo2030@gmail.com>
2024-01-12 14:14:52 +0630

    adding the whole project files and image

--- app.js
+++ app.js
@@ -1,7 +1,28 @@
+let getheader = document.getElementById('header');
 
+        function shownav(){
+          getheader.classList.toggle('navheader');
+        }
+
+        let navbar = document.getElementById('navbar');
+        
+        function  removenav(){
+            if(getheader.classList.contains('navheader')){
+                // console.log("this is small size");
+                getheader.className = "";
+            }else {
+                // console.log("This is normal");
+            }
+        }
+    
+
+
+// This is form 
+const form = document.getElementById('form');
 const email = document.getElementById("email");
 const input = document.querySelectorAll("input");
-// const validityState = input.validity;
+const message = document.getElementById('message');
+console.log(input[0].value);
 
                                                                                                   
 email.addEventListener("input", (event) => {
@@ -12,6 +33,23 @@ email.addEventListener("input", (event) => {
     }
   });
 
+  document.querySelector('form').onsubmit = e => {
+    e.target.submit();
+    e.target.reset();
+    return false;
+ };
+
+
+//   function submitform(){
+//     // form.submit();
+//     input[0].value = '';
+//     input[1].value = '';
+//     message.value = '';
+// }
+//   }
+
+// function cleardata(){
+  
 
 
 
